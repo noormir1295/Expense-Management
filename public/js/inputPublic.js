23350c8edc2af9968c54a15e7f6ed85cd065450d
grrazzlle <cel47@miami.edu>
2020-10-14 22:31:23 -0400

    fixed date and input nav bug

--- public/js/inputPublic.js
+++ public/js/inputPublic.js
@@ -12,13 +12,18 @@ $(document).ready(function() {
   const submitBtn = $('#submit-btn');
 
   $(submitBtn).on('click', function inputSubmit(event) {
-
     event.preventDefault();
-    
+    console.log(inputDate.val().trim(),)
+    let year = inputDate.val().trim().slice(0, 4);
+    let month = inputDate.val().trim().slice(5, 7);
+    let day = inputDate.val().trim().slice(8,10);
+    dateTotal = month+"/"+day+"/"+year
+    console.log(dateTotal)
+
     const newExpense = {
       title: inputTitle.val().trim(),
       amount: inputAmount.val().trim(),
-      date: inputDate.val().trim(),
+      date: dateTotal,
       optionalText: inputOptionalText.val().trim(),
       CategoryId: localCategorySave
     }
